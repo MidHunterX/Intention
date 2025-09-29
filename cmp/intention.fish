1082da8b39bd27f129bd006f29039be10c309cff
MidHunterX <midhunterx@gmail.com>
2025-09-29 13:45:31 +0530

    🛺 feat(cmp): add autocompletion for fish shell

---
+++ cmp/intention.fish
@@ -0,0 +1,15 @@
+# Completion for `intention` command
+# This file should be in: ~/.config/fish/completions/intention.fish
+
+# Disable file completion for `intention` command
+complete -c intention -f
+
+complete -c intention -n '__fish_use_subcommand' -a list -d 'List all available intents'
+complete -c intention -n '__fish_use_subcommand' -a run -d 'Execute your intention immediately'
+complete -c intention -n '__fish_use_subcommand' -a set -d 'Set your intention for later use'
+complete -c intention -n '__fish_use_subcommand' -a unset -d 'Unset your intention for later use'
+
+
+complete -c intention -n '__fish_seen_subcommand_from set' -a '(intention list)' -d 'intent'
+complete -c intention -n '__fish_seen_subcommand_from unset' -a '(intention list)' -d 'intent'
+complete -c intention -n '__fish_seen_subcommand_from run' -a '(intention list)' -d 'intent'
